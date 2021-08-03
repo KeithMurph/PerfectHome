facb398fc0142050e884a32bb6818a74ccf37b70
Meghan Andrewartha <megandrew1918@gmail.com>
2021-08-03 14:31:44 -0700

    adding all the seeds

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const db = require('../models');
 const withAuth = require('../utils/auth');
 
+// router.get("/", (req,res) => {
+//     db.Post.findAll({
+//         //finding user info with the posts
+//         include: [db.User]
+//     }).then(data => {
+//         const allPost = data.map(post=> post.get({
+//             plain:true
+//         }))
+//         res.render("allPost", {layout:"dashboard", allPost})
+//     })
+// })
+
 router.get("/login", (req,res) =>{
     if(req.session.loggedIn){
       res.redirect("/")  
---
+++ models/Adoptable.js
@@ -0,0 +1,75 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Adoptable extends Model {}
+
+Adoptable.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    description: {
+        type:DataTypes.STRING
+    },
+    type:{
+        type:DataTypes.STRING
+    },
+    breed:{
+        type:DataTypes.STRING
+    },
+    size: {
+        type:DataTypes.STRING
+    },
+    age:{
+        type:DataTypes.STRING
+    },
+    house_size:{
+        type:DataTypes.STRING
+    },
+    good_with_children:{
+        type:DataTypes.BOOLEAN
+    },
+    good_with_dogs:{
+        type:DataTypes.BOOLEAN
+    },
+    good_with_cats:{
+        type:DataTypes.BOOLEAN
+    },
+    has_yard:{
+        type:DataTypes.BOOLEAN
+    },
+    has_claws:{
+        type:DataTypes.BOOLEAN
+    },
+    spayed:{
+        type:DataTypes.BOOLEAN
+    },
+    special_needs:{
+        type:DataTypes.BOOLEAN
+    },
+    image_url:{
+        type:DataTypes.STRING
+    },
+    user_id:{
+        type:DataTypes.INTEGER,
+        references: {
+            model: "user",
+            key: "id"
+          }
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'preferences'
+})
+
+module.exports = Preferences

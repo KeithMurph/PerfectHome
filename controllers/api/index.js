bb19458ff2b3dcacef373973906244e2d47d5a05
Meghan Andrewartha <megandrew1918@gmail.com>
2021-08-06 14:39:37 -0700

    adding route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,13 @@ const router = require('express').Router();
 const findAnimalsRoutes = require("./findAnimalsRoutes")
 const userRoutes = require("./userRoutes")
 const dogBreedRoutes = require("./dogBreedRoutes")
+const adoptableRoutes = require("./adoptableRoutes");
+
 
 router.use("/findAnimals", findAnimalsRoutes)
 router.use("/users", userRoutes)
 router.use("/breeds", dogBreedRoutes)
+router.use("/adoptable", adoptableRoutes)
 
 
 module.exports = router;
